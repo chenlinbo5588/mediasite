434e075f6ffd237b6155efa53a7604c1159a5dbf
chenlinbo <clb@localhost.localdomain>
2012-11-14 17:12:32 +0800

    初始化话 ThinkPHP 以及应用入口index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * APP Entry file 
+ *
+ */
+
+define('APP_DEBUG',true);
+define('APP_NAME', 'www.mediasite.com');
+define('APP_PATH',dirname(__FILE__).'/'.APP_NAME.'/');
+
+require './ThinkPHP/ThinkPHP.php';
