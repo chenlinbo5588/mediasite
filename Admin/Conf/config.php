c358fb5dcd5656240c1e1c281537536d4c86befe
bella <lichungaojk@gmail.com>
2012-11-24 13:31:40 +0800

    切割页面

--- Admin/Conf/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$siteConfig         = array();
-$siteConfigFile     = ROOT_PATH . DS . 'config.inc.php';
-if(is_file($siteConfigFile)) {
-    $siteConfig = require $siteConfigFile;
-}
-return array_merge($siteConfig, $appConfig);
-?>
---
+++ Index/Lib/Action/AdminAction.class.php
@@ -0,0 +1,11 @@
+<?php
+class AdminAction extends CommonAction {
+    public function index(){
+        $this->display();
+    }
+
+    public function user(){
+        $this->display();
+    }
+
+}
