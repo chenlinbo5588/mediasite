38b318f4f1c8631db7d978de3d9c066b0902a1ea
bella <lichungaojk@gmail.com>
2012-11-25 21:10:33 +0800

    添加JS弹出窗

---
+++ Public/Javascript/common.js
@@ -0,0 +1,24 @@
+$(function() {
+	$( "#dialog" ).dialog({
+			autoOpen: false,
+			width: 400,
+			modal:true,
+			buttons: [
+				{
+					text: "Save",
+					click: function() {
+						$( this ).dialog( "close" );
+					}
+				},
+				{
+					text: "Cancel",
+					click: function() {
+						$( this ).dialog( "close" );
+					}
+				}
+			]
+		});
+	$( "#dialog-link" ).click(function( event ) {
+			$( "#dialog" ).dialog( "open" );
+	});
+});
