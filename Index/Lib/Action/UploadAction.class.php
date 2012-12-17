1c322636c68046f0bd04d0bfbd8fe4de3fdbcdaf
chenlinbo <chenlinbo5588@163.com>
2012-12-17 23:43:04 +0800

    上传

--- Index/Lib/Action/UploadAction.class.php
+++ Index/Lib/Action/UploadAction.class.php
@@ -15,7 +15,22 @@ class UploadAction extends CommonAction {
          * 上传处理
          */
         
+        $attachment = array(
+            'filename'  => $_FILES['Filedata']['name'],
+            'filesize'  => $_FILES['Filedata']['size'],
+            'type'		=> $_FILES["Filedata"]["type"]
+        );
         
+        /**
+         *@todo db insert  
+         */
+        
+        /**
+         * 服务器path 
+         */
+        
+        echo json_encode(array('respcode' => 0,'id'=>1,'path' => 'file/1/1.mp3',"filename" => $attachment['filename']));
+
         
     }
 
