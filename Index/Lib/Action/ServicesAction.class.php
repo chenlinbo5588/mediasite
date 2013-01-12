41f4c90dc85fda4544928bb73711598e585cf459
bella <lichungaojk@gmail.com>
2013-01-12 20:04:12 +0800

    添加Services模块，修改菜单样式

---
+++ Index/Lib/Action/ServicesAction.class.php
@@ -0,0 +1,10 @@
+<?php
+class ServicesAction extends CommonAction {
+    public function index(){
+        $this->display();
+    }
+
+    public function production(){
+        $this->display();
+    }
+}
